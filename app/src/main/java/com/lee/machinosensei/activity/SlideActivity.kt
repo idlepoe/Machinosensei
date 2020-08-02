c1f08247b73c33de1373979a663b8e27762cfbe1
heejung <leehj8230@gmail.com>
2020-08-02 11:45:13 +0900

    슬라이드 추가/어뎁터,모델→HomeFragment이동/어뎁터패키지삭제

--- app/src/main/java/com/lee/machinosensei/activity/SlideActivity.kt
+++ app/src/main/java/com/lee/machinosensei/activity/SlideActivity.kt
@@ -3,10 +3,24 @@ package com.lee.machinosensei.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.lee.machinosensei.R
+import kotlinx.android.synthetic.main.activity_slide.*
+import kotlinx.android.synthetic.main.item_intropager.view.*
 
 class SlideActivity : AppCompatActivity() {
+
+    private var pageItemList = ArrayList<PageItem>()
+    private  lateinit var introPagerAdapter: IntroPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slide)
@@ -18,5 +32,51 @@ class SlideActivity : AppCompatActivity() {
                 HomeActivity::class.java)
             startActivity(intent)
         }
+
+        pageItemList.add(PageItem(R.color.slideBg1, R.drawable.ic_menu_gallery,"content1"))
+        pageItemList.add(PageItem(R.color.slideBg2, R.drawable.ic_menu_camera,"content2"))
+        pageItemList.add(PageItem(R.color.slideBg3, R.drawable.ic_menu_slideshow,"content3"))
+
+        introPagerAdapter = IntroPagerAdapter(pageItemList)
+
+        vpIntro.apply {
+            adapter = introPagerAdapter
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            dots_indicator.setViewPager2(this)
+        }
+//        vpIntro.adapter = introPagerAdapter
+//        vpIntro.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    class IntroPagerAdapter(private var pageList: ArrayList<PageItem>)
+        : RecyclerView.Adapter<IntroPagerHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroPagerHolder {
+            return IntroPagerHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_intropager, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return pageList.size
+        }
+
+        override fun onBindViewHolder(holder: IntroPagerHolder, position: Int) {
+            holder.bindwithView(pageList[position])
+        }
+
+    }
+
+    class IntroPagerHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val itemIage : ImageView = itemView.ivImage
+        private val itemContent : TextView = itemView.tvContent
+        private val itemBg : LinearLayout = itemView.pagerBg
+
+        fun bindwithView(pageItem: PageItem) {
+            itemIage.setImageResource(pageItem.imageSrc)
+            itemContent.text = pageItem.content
+            itemBg.setBackgroundResource(pageItem.bgColol)
+        }
+    }
+
+    class PageItem(val bgColol: Int, val imageSrc: Int, val content: String) {
     }
 }
--- app/src/main/java/com/lee/machinosensei/fragment/HomeFragment.kt
+++ app/src/main/java/com/lee/machinosensei/fragment/HomeFragment.kt
@@ -7,18 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.lee.machinosensei.R
-import com.lee.machinosensei.adapter.TeacherGrid
-import com.lee.machinosensei.adapter.TeacherGridAdapter
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.item_teacher.view.*
 
 
 class HomeFragment : Fragment() {
 
     var adapter: TeacherGridAdapter? = null
     var adapter_area: TeacherGridAdapter? = null
-    var teacherList = ArrayList<TeacherGrid>()
-    var teacherList_area = ArrayList<TeacherGrid>()
+    var teacherList = ArrayList<Teacher>()
+    var teacherList_area = ArrayList<Teacher>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,45 +29,45 @@ class HomeFragment : Fragment() {
         // TODO DBから先生情報を取得しLISTにセットする
         //  　イメージは何型？
         var drawable: Drawable = resources.getDrawable(R.drawable.ic_menu_camera,null)
-        teacherList.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList.add(TeacherGrid("Espersso", drawable, "detail"))
-        teacherList.add(TeacherGrid("French Fires", drawable, "detail"))
-        teacherList.add(TeacherGrid("Honey",drawable, "detail"))
-        teacherList.add(TeacherGrid("Strawberry", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Strawberry", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Honey", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Honey", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Honey", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
-        teacherList.add(TeacherGrid("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Coffee", drawable, "detail"))
+        teacherList.add(Teacher("Espersso", drawable, "detail"))
+        teacherList.add(Teacher("French Fires", drawable, "detail"))
+        teacherList.add(Teacher("Honey",drawable, "detail"))
+        teacherList.add(Teacher("Strawberry", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Strawberry", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Honey", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Honey", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Honey", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
+        teacherList.add(Teacher("Sugar cubes", drawable, "detail"))
         adapter = TeacherGridAdapter(teacherList)
 
         val root = inflater.inflate(R.layout.fragment_home, container, false)
@@ -80,16 +80,16 @@ class HomeFragment : Fragment() {
             Toast.makeText(this.requireContext(), "先生詳細へ", Toast.LENGTH_SHORT).show()
         })
 
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
-        teacherList_area.add(TeacherGrid("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
+        teacherList_area.add(Teacher("Coffee", drawable, "detail"))
         adapter_area = TeacherGridAdapter(teacherList_area)
         // 近所の先生
         root.recycler_area.adapter = adapter_area
@@ -100,5 +100,52 @@ class HomeFragment : Fragment() {
         })
 
         return root
+    }
+
+    class Teacher {
+        var ivTeacher: Drawable? = null
+        var tvTeacherName: String? = null
+        var tvdetail1: String? = null
+
+        constructor(name: String, image: Drawable, detail1: String) {
+            this.ivTeacher = image
+            this.tvTeacherName = name
+            this.tvdetail1 = detail1
+
+        }
+    }
+
+    class TeacherGridAdapter(private val items: ArrayList<Teacher>) :
+        RecyclerView.Adapter<TeacherGridAdapter.ViewHolder>() {
+
+        override fun getItemCount() = items.size
+
+        override fun onBindViewHolder(holder: TeacherGridAdapter.ViewHolder, position: Int) {
+            val item = items[position]
+            val listener = View.OnClickListener {it ->
+                Toast.makeText(it.context, "Clicked: ${item.tvTeacherName}", Toast.LENGTH_SHORT).show()
+            }
+            holder.apply {
+                bind(listener, item)
+                itemView.tag = item
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+                TeacherGridAdapter.ViewHolder {
+            val inflatedView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_teacher, parent, false)
+            return TeacherGridAdapter.ViewHolder(inflatedView)
+        }
+
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+            private var view: View = v
+            fun bind(listener: View.OnClickListener, item: Teacher) {
+                view.ivTeacher.setImageDrawable(item.ivTeacher)
+                view.tvTeacherName.text = item.tvTeacherName
+                view.tvDetail1.text = item.tvdetail1
+                view.setOnClickListener(listener)
+            }
+        }
     }
 }
